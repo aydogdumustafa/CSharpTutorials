7c94a7237c0149e6073b1f0c7df9dc23b818e460
aydogdumustafa <69818194+aydogdumustafa@users.noreply.github.com>
2021-05-04 18:12:37 +0200

    Added Dictionaries

---
+++ Dictionaries/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Dictionaries
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> capitals = new Dictionary<string, string>();
+            capitals.Add("Turkey", "Ankara");
+            capitals.Add("Poland", "Warsaw");
+            capitals.Add("Germany", "Berlin");
+            capitals.Add("England", "London");
+
+            Console.WriteLine("Capital : {0}", capitals["Poland"]);
+            
+        }
+    }
+}
