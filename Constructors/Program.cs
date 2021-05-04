213f7d7f207889a0267e0224d8eb04b6a70d8b50
aydogdumustafa <69818194+aydogdumustafa@users.noreply.github.com>
2021-05-04 17:09:03 +0200

    Add project files.

---
+++ Constructors/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Constructors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer1 = new Customer {Id= 1 , FirstName="Mustafa",LastName ="Aydogdu", City = "Warszawa"};
+
+            Customer customer2 = new Customer(2,"Emre","Aydogdu","Konya");
+
+            Customer customer3 = new Customer();
+            customer3.Id = 3;
+            customer3.FirstName = "Ayse";
+            customer3.LastName = "Aydogdu;";
+            customer3.City = "Trabzon";
+
+            Console.WriteLine(customer2.FirstName);
+        }
+       
+    }
+
+    class Customer
+    {
+        //default constructor
+        public Customer()
+        {
+
+        }
+        public Customer(int id, string firstName, string lastName, string city)
+        {
+            Id = id;
+            FirstName = firstName;
+            LastName = lastName;
+            City = city;
+        }
+
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string City { get; set; }
+
+    }
+}
