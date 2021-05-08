f3abe99b2cc93d9340f92b4969bc26f7ec585a9d
aydogdumustafa <69818194+aydogdumustafa@users.noreply.github.com>
2021-05-08 16:07:18 +0200

    Demo on Interdafe

---
+++ Interfaces2/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+
+namespace Interfaces2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //IPersonManager customerManager = new CustomerManager();
+            
+
+            //IPersonManager employeeManager = new EmployeeManager();
+            
+            ProjectManager projectManager = new ProjectManager();
+            projectManager.Add(new CustomerManager());
+            projectManager.Add(new EmployeeManager());
+            projectManager.Add(new InternManager());
+
+
+        }
+    }
+    //class PersonManager
+    //{
+    //    //implemented operation
+    //    public void Add()
+    //    {
+    //        Console.WriteLine("Eklendi");
+    //    }
+    //}
+    interface IPersonManager
+    {
+        //unimplemented operation
+        void Add();
+        void Update();
+
+
+    }
+    class CustomerManager : IPersonManager
+    {
+        public void Add()
+        {
+            Console.WriteLine("Customer Added");
+        }
+
+        public void Update()
+        {
+            Console.WriteLine("Customer Updated");
+        }
+    }
+    //class inherits ---------------- interface implements
+
+    class EmployeeManager : IPersonManager
+    {
+        public void Add()
+        {
+            Console.WriteLine("Employee Added");
+        }
+
+        public void Update()
+        {
+            Console.WriteLine("Employee Updated");
+        }
+    }
+    class InternManager : IPersonManager
+    {
+        public void Add()
+        {
+            Console.WriteLine("Intern Added");
+        }
+
+        public void Update()
+        {
+            Console.WriteLine("Intern Updated");
+        }
+    }
+
+    class ProjectManager
+    {
+        public void Add(IPersonManager personManager)
+        {
+            personManager.Add();
+        }
+      
+    }
+    
+
+    class Person
+    {
+
+    }
+    class Customer : Person
+    {
+
+    }
+}
