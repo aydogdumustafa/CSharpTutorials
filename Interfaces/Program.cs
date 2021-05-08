7db52625e5be3331a3a4f3747eb04548988ad69e
aydogdumustafa <69818194+aydogdumustafa@users.noreply.github.com>
2021-05-08 13:00:53 +0200

    Using of Interfeces

---
+++ Interfaces/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Interfaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         
+        }
+    }
+    interface IPerson
+    {
+        int Id { get; set; }
+        string FirstName { get; set; }
+        string LastName { get; set; }
+
+    }
+    class Customer : IPerson
+    {
+        public int Id { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string FirstName { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string LastName { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+       
+        public string Adress { get; set; }
+    }
+
+    class Student : IPerson
+    {
+        public int Id { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string FirstName { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string LastName { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+       
+        public string Departmant { get; set; }
+    }
+}
